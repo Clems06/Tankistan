61074f12e1baffa2fbd0e3e046bc18f91e3ae072
Clément Genninasca <clementgenni@gmail.com>
2023-08-22 21:03:34 +0200

    action add updated

--- setup/add_actions.php
+++ setup/add_actions.php
@@ -1,5 +1,5 @@
 <?php
 require_once "../config.php";
 
-$all_users = mysqli_query($link, "UPDATE tanks SET actions = actions + 1 WHERE health > 0;");
+$all_users = mysqli_query($link, "UPDATE tanks SET actions = actions + 1 WHERE health > 0 AND x>0;");
 ?>
